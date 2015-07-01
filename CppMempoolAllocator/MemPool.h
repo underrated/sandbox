40f27cde417febec4fd0d9b91f597bc65db877cb
root <root@underrated.(none)>
2015-07-01 00:26:26 -0400

    added api to MemPool

--- CppMempoolAllocator/MemPool.h
+++ CppMempoolAllocator/MemPool.h
@@ -1,9 +1,20 @@
 #ifndef __MEMPOOL__
 
+#include <cstdlib>
+#include <iostream>
+#include <new>
+
 class MemPool {
-    public:
-    MemPool();
-    ~MemPool();
+	public:
+		MemPool();
+		~MemPool();
+		
+		void* allocate(size_t s);
+		int deallocate(void* ptr);
+
+	private:
+		// List of allocated blocks
+		// List of unallocated blocks
 };
 
 #endif // __MEMPOOL__
--- myWidgets/myTreeView/events.js
+++ myWidgets/myTreeView/events.js
@@ -13,22 +13,22 @@ Event.prototype.addListener = function(cb, context) {
         "cb": cb,
         "context":context
     });
-}
+};
 
 Event.prototype.emit = function(data) {
     for(var i=0;i<this.listeners.length;i++)
         this.listeners[i].cb.call(context, data);
-}
+};
 
 
 function EventInterface() {}
 EventInterface.prototype.emitEvent = function (e,data) {
     if(!Events[e]) Events[e] = new Event(e);
     Events[e].emit(data);
-}
+};
 
 EventInterface.prototype.onEvent = function (e, cb) {
     if(!Events[e]) Events[e] = new Event(e);
     Events[e].addListener(cb, this);
-}
+};
 
